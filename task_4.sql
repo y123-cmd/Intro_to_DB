2f9ee9349c69d56da6dd3628687245a779c5c10f
Yvonne Bosire <yvonnebosire24@gmail.com>
2024-10-20 23:20:42 +0300

    Add task_4.sql: script to print full description of Books table

---
+++ task_4.sql
@@ -0,0 +1,12 @@
+SELECT 
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM 
+    information_schema.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
