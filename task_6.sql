b610e8175527e0e8bb14cb62cd1a7a2ff5d8e636
Yvonne Bosire <yvonnebosire24@gmail.com>
2024-10-21 01:52:21 +0300

    Added task_6.sql for customer data insertion

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,5 @@
 -- task_6.sql
 
-USE alx_book_store;
-
 INSERT INTO customer (customer_id, customer_name, email, address) VALUES 
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
