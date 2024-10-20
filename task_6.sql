9f6f7343e8c29da84e8c149b3174ca1716a13ffd
Yvonne Bosire <yvonnebosire24@gmail.com>
2024-10-21 01:00:20 +0300

    Added task_6.sql with customer data insertion

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,18 @@
+USE alx_book_store;
+
+DROP TABLE IF EXISTS Customers;
+
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(255),
+    email VARCHAR(255),
+    address VARCHAR(255)
+);
+
 INSERT INTO Customers (customer_id, customer_name, email, address) VALUES 
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
 (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+
+SELECT * FROM Customers;
 
